18c7738086ee8e06f0753e303e85794951d51c69
kartikay-28 <kartikayrattan1@gmail.com>
2025-04-26 10:54:44 +0530

    Done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const PORT = 3000;
 const app = express();
 const User = require('./schema');
 
-app.use = express.json();
+app.use(express.json());
 
 
 app.get('/',async (req,res)=>{
@@ -15,30 +15,28 @@ app.post('/signup',async (req,res)=>{
         const{username , email , password , DateofBirth} = req.body;
         
         if(!username){
-            res.status(400).send("username can  not be empty");
+            res.status(400).json({message:"username can  not be empty"});
         }
         if(!email){
-            res.status(400).send("email cannot be empty");
+            res.status(400).json({message:"email cannot be empty"});
         }
         if(!password && 8<=password.length()<=16){
-            res.status(400).send("password length should be greater than or equal to 8 or less than or equal to 16");
+            res.status(400).json({message:"password length should be greater than or equal to 8 or less than or equal to 16"});
         }
         if(!DateofBirth){
-            res.status(400).send("DateofBirth cannot be empty");
+            return res.status(400).json({message:"DateofBirth can not be empty"})
         }
         const newuser = User.create({username,email,password,DateofBirth})
         await newuser.save();
 
-        res.status(201).send("User created succesfully",newuser);
+        res.status(201).json({message:"User created succesfully" , user:newuser});
 
     }
-    catch(error){
-        if(!newuser){
-            res.status(500).error(message = "Server error", error)
-        }
-    }
+    catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+      }
 })
 
-app.listen(PORT,async(req,res) => {
+app.listen(PORT,()=>{
 console.log(`Server is running on http://localhost:${PORT}`)
 })
